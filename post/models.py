ac43ebb4ba33026fd13bf193fa48fc4d9b54a5a5
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-07-28 17:34:14 +0600

    create list and detail view for post

--- post/models.py
+++ post/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset()\
+                                            .filter(status='published')
+
+
 class Post(models.Model):
     """Blog post object"""
     STATUS_CHOICES = (
@@ -21,6 +28,8 @@ class Post(models.Model):
     status = models.CharField(max_length=10,
                               choices=STATUS_CHOICES,
                               default='draft')
+    objects = models.Manager()  # The default manager.
+    published = PublishedManager()  # Our custom manager.
 
     class Meta:
         ordering = ('-publish',)
