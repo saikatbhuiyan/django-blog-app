1e0f02c979f9889029a91273e445f0ba4ed7c7c8
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-07-29 11:07:53 +0600

    create and detail view

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls', namespace='blog')),
+    path('blog/', include('post.urls', namespace='post')),
 ]
--- post/models.py
+++ post/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 from django.contrib.auth.models import User
 
 
@@ -36,3 +37,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day, self.slug])
