d53bf7d4e3ef743a1bb6cbc161657054453c4027
saikatbhuiyan <shaikotbhuiya@gmail.com>
2020-08-13 01:59:15 +0600

    builtin login logout

--- blog/settings.py
+++ blog/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join('templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
